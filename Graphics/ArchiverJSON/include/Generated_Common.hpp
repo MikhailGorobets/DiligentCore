f485d58a2fcbcaf4a611a5df99410e631820036e
MikhailGorobets <misha.friman@mail.ru>
2021-11-11 20:57:06 +0500

    Serialization: Added JSON serialization of the Graphics Pipline

---
+++ Graphics/ArchiverJSON/include/Generated_Common.hpp
@@ -0,0 +1,111 @@
+/*
+ *  Copyright 2019-2021 Diligent Graphics LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  In no event and under no legal theory, whether in tort (including negligence),
+ *  contract, or otherwise, unless required by applicable law (such as deliberate
+ *  and grossly negligent acts) or agreed to in writing, shall any Contributor be
+ *  liable for any damages, including any direct, indirect, special, incidental,
+ *  or consequential damages of any character arising as a result of this License or
+ *  out of the use or inability to use the software (including but not limited to damages
+ *  for loss of goodwill, work stoppage, computer failure or malfunction, or any and
+ *  all other commercial damages or losses), even if such Contributor has been advised
+ *  of the possibility of such damages.
+ */
+
+#pragma once
+
+namespace Diligent
+{
+
+template <typename T>
+inline T** remove_const(const T** x)
+{
+    return const_cast<T**>(x);
+}
+
+inline const char* copy_string(const std::string& Str)
+{
+    return EngineEnvironment::GetInstance()->GetMemoryAllocator()->CopyString(Str);
+}
+
+template <typename Type>
+inline void to_json_ptr(nlohmann::json& Json, const Type* pData, size_t Size)
+{
+    for (size_t i = 0; i < Size; i++)
+        Json.push_back(pData[i]);
+}
+
+template <typename Type>
+inline void from_json_ptr(const nlohmann::json& Json, Type** pObjects, size_t Size)
+{
+    auto* pData = reinterpret_cast<Type*>(EngineEnvironment::GetInstance()->GetMemoryAllocator()->Allocate<Type>(Size));
+    for (size_t i = 0; i < Size; i++)
+        pData[i] = Json[i].get<Type>();
+    *pObjects = pData;
+}
+
+template <typename Type>
+inline void to_json_ptr(nlohmann::json& Json, const Type* pObject)
+{
+    Json = *pObject;
+}
+
+template <typename Type>
+inline void from_json_ptr(const nlohmann::json& Json, Type** pObject)
+{
+    *pObject  = reinterpret_cast<Type*>(EngineEnvironment::GetInstance()->GetMemoryAllocator()->Allocate<Type>());
+    **pObject = Json.get<Type>();
+}
+
+template <>
+inline void to_json_ptr(nlohmann::json& Json, const void* pData, size_t Size)
+{
+    std::vector<uint8_t> PackedData(static_cast<const uint8_t*>(pData), static_cast<const uint8_t*>(pData) + Size);
+    Json = nlohmann::json::binary(PackedData);
+}
+
+template <>
+inline void from_json_ptr(const nlohmann::json& Json, void** pObject, size_t Size)
+{
+    uint8_t* pData = static_cast<uint8_t*>(EngineEnvironment::GetInstance()->GetMemoryAllocator()->Allocate(Size, alignof(Uint32)));
+    std::memcpy(pData, Json.get_binary().data(), Size);
+    *pObject = pData;
+}
+
+template <typename T>
+inline void to_json_bitwise(nlohmann::json& Json, T EnumBits)
+{
+    auto BitArray = nlohmann::json::array();
+    for (Uint32 Bits = EnumBits; Bits != 0;)
+    {
+        const auto Type = static_cast<const T>(ExtractLSB(Bits));
+        BitArray.push_back(Type);
+    }
+    Json = BitArray.size() > 1 ? BitArray : EnumBits;
+}
+
+template <typename T>
+inline void from_json_bitwise(const nlohmann::json& Json, T& EnumBits)
+{
+    auto ExtractBits = [](auto Array) -> T {
+        T Bits = {};
+        for (auto const& Bit : Array)
+            Bits |= Bit.get<T>();
+        return Bits;
+    };
+    EnumBits = Json.is_array() ? ExtractBits(Json) : Json.get<T>();
+}
+
+} // namespace Diligent
---
+++ Graphics/ArchiverJSON/src/DeviceObjectReflection.cpp
@@ -0,0 +1,100 @@
+/*
+ *  Copyright 2019-2021 Diligent Graphics LLC
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  In no event and under no legal theory, whether in tort (including negligence),
+ *  contract, or otherwise, unless required by applicable law (such as deliberate
+ *  and grossly negligent acts) or agreed to in writing, shall any Contributor be
+ *  liable for any damages, including any direct, indirect, special, incidental,
+ *  or consequential damages of any character arising as a result of this License or
+ *  out of the use or inability to use the software (including but not limited to damages
+ *  for loss of goodwill, work stoppage, computer failure or malfunction, or any and
+ *  all other commercial damages or losses), even if such Contributor has been advised
+ *  of the possibility of such damages.
+ */
+
+
+#include "pch.h"
+
+namespace Diligent
+{
+
+void DeviceObjectReflection::Serialize(nlohmann::json& Json, const IRenderPass* pDeviceObject)
+{
+    auto& ResourceDesc = pDeviceObject->GetDesc();
+    VERIFY_EXPR(ResourceDesc.Name != nullptr);
+    Json = pDeviceObject->GetDesc();
+}
+
+void DeviceObjectReflection::Deserialize(const nlohmann::json& Json, IRenderPass** pDeviceObject)
+{
+    auto pDevice      = EngineEnvironment::GetInstance()->GetSerializationDevice();
+    auto ResourceDesc = Json.get<RenderPassDesc>();
+    pDevice->CreateRenderPass(ResourceDesc, pDeviceObject);
+
+    if (!pDeviceObject)
+        LOG_ERROR_AND_THROW("Failed to create RenderPass -> '", ResourceDesc.Name, "'.");
+}
+
+void DeviceObjectReflection::Serialize(nlohmann::json& Json, const IShader* pDeviceObject)
+{
+    auto& ResourceDesc = pDeviceObject->GetDesc();
+    VERIFY_EXPR(ResourceDesc.Name != nullptr);
+    Json = pDeviceObject->GetDesc();
+}
+
+void DeviceObjectReflection::Deserialize(const nlohmann::json& Json, IShader** pDeviceObject)
+{
+    auto pEnvironment = EngineEnvironment::GetInstance();
+    auto pDevice      = pEnvironment->GetSerializationDevice();
+    auto pStream      = pEnvironment->GetShaderSourceInputStreamFactory();
+    auto ResourceDesc = Json.get<ShaderCreateInfo>();
+
+    ResourceDesc.pShaderSourceStreamFactory = pStream;
+    pDevice->CreateShader(ResourceDesc, pEnvironment->GetDeviceBits(), pDeviceObject);
+    if (!pDeviceObject)
+        LOG_ERROR_AND_THROW("Failed to create Shader -> '", ResourceDesc.Desc.Name, "'.");
+}
+
+void DeviceObjectReflection::Serialize(nlohmann::json& Json, IPipelineResourceSignature** pDeviceObjects, size_t Size)
+{
+    for (size_t i = 0; i < Size; i++)
+    {
+        auto& ResourceDesc = pDeviceObjects[i]->GetDesc();
+        VERIFY_EXPR(ResourceDesc.Name != nullptr);
+        Json.push_back(ResourceDesc);
+    }
+}
+
+void DeviceObjectReflection::Deserialize(const nlohmann::json& Json, IPipelineResourceSignature*** pDeviceObjects, size_t Size)
+{
+    auto pEnvironment = EngineEnvironment::GetInstance();
+    auto pAllocator   = pEnvironment->GetMemoryAllocator();
+    auto pDevice      = pEnvironment->GetSerializationDevice();
+
+    auto pData = reinterpret_cast<IPipelineResourceSignature**>(pAllocator->Allocate<IPipelineResourceSignature*>(Size));
+    for (size_t i = 0; i < Size; i++)
+    {
+        IPipelineResourceSignature* pDeviceObject;
+        auto                        ResourceDesc = Json[i].get<PipelineResourceSignatureDesc>();
+        pDevice->CreatePipelineResourceSignature(ResourceDesc, pEnvironment->GetDeviceBits(), &pDeviceObject);
+        if (!pDeviceObject)
+            LOG_ERROR_AND_THROW("Failed to create Resource Signature -> '", ResourceDesc.Name, "'.");
+        pData[i] = pDeviceObject;
+    }
+    *pDeviceObjects = pData;
+}
+
+
+} // namespace Diligent
